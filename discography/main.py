99736b2af1f3d10bd11c3e568b97079b0aade6b9
Pramit Kumar Bhaduri <47381380+PramitKumar27@users.noreply.github.com>
2023-02-03 15:03:45 +0100

    update

--- discography/main.py
+++ discography/main.py
@@ -11,21 +11,21 @@ url = "https://en.wikipedia.org/wiki/"+word
 url1 = "http://en.wikipedia.org/wiki/"+word+"_discography#Studio_albums"
 
 
-def info(url):
-    url_open = requests.get(url)
-    soup = BeautifulSoup(url_open.content, "html.parser")
-    details = soup('table', {'class': 'infobox'})
-    for i in details:
-        h = i.find_all('tr')
-        for j in h:
-            heading = j.find_all('th')
-            detail = j.find_all('td')
-            if heading is not None and detail is not None:
-                for x, y in zip(heading, detail):
-                    print("{} :: {}".format(x.text, y.text))
-                    print("-------------------------------")
-    for i in range(1, 3):
-        print(soup('p')[i].text)
+# def info(url):
+#     url_open = requests.get(url)
+#     soup = BeautifulSoup(url_open.content, "html.parser")
+#     details = soup('table', {'class': 'infobox'})
+#     for i in details:
+#         h = i.find_all('tr')
+#         for j in h:
+#             heading = j.find_all('th')
+#             detail = j.find_all('td')
+#             if heading is not None and detail is not None:
+#                 for x, y in zip(heading, detail):
+#                     print("{} :: {}".format(x.text, y.text))
+#                     print("-------------------------------")
+#     for i in range(1, 3):
+#         print(soup('p')[i].text)
 
 
 def disco(url1):
@@ -53,10 +53,10 @@ def disco(url1):
 
 if __name__ == "__main__":
 
-    inp = input("Do you want to know basic info? Y/N ")
-    if inp == "Y":
-        print("Here is the basic info regarding: {0}".format(word))
-        info(url)
+    # inp = input("Do you want to know basic info? Y/N ")
+    # if inp == "Y":
+    #     print("Here is the basic info regarding: {0}".format(word))
+    #     info(url)
     inp1 = input("Do you want to know discography? Y/N ")
     if inp1 == "Y":
         print("Here is {0}'s discography :: ".format(word))
