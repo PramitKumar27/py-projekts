54cc757c13badca40c455ecc6d2c36bd9e954759
Pramit Kumar Bhaduri <47381380+PramitKumar27@users.noreply.github.com>
2023-02-10 15:53:09 +0100

    update

--- discography/main.py
+++ discography/main.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import string
 
-s0 = input("Search for : ")
+s0 = input("Search for :  ")
 s = string.capwords(s0)
 s1 = s.split()
 word = "_".join(s1)
