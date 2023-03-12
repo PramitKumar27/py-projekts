29c6c5550a216bcb85bcd5f6a7d6ad80e7574ae8
Pramit Kumar Bhaduri <47381380+PramitKumar27@users.noreply.github.com>
2023-03-12 21:01:28 +0100

    update

--- word_finder/main.py
+++ word_finder/main.py
@@ -1,8 +1,5 @@
-import os
-os.chdir('C:/Users/prami/OneDrive/Desktop/py-projekts/word_finder')
+from helpers import *
 
-files_path = os.path.join(os.curdir, 'words_directory')
 
-files_full_path = [os.path.join(files_path, f) for f in os.listdir(files_path)]
-
-print(files_full_path)
+def main():
+    return 0
